c9a06142862c95d3cd79db3c2cf3a3044d128101
Marc-Antoine <marc-antoine.guery@etu.univ-lyon1.fr>
2019-10-06 17:54:43 +0200

    Medal per top countries graph

--- medana.R
+++ medana.R
@@ -4,6 +4,8 @@ library(reshape2)
 library(stringr)
 library(ggplot2)
 library(plotly)
+library(ade4)
+library(factoextra)
 
 options(stringsAsFactors = FALSE)
 #################MEDALS########################
@@ -33,33 +35,54 @@ medasplit$Date <- sub(pattern = "\\D$", replacement = "", medasplit$Date)
 medasplit$Season[medasplit$Season=="w"] <- "Winter"
 medasplit$Season[medasplit$Season!="Winter"] <- "Summer"
 
-countries <- read.csv("olympic-cities.csv", stringsAsFactors = F)
+countries <- read.csv("olympic-cities.csv")
 colnames(countries)[3] <- "HostCountry"
 medasplit <- merge(medasplit, countries, by = c("Date", "Season"))
-rm(ok)
+#################FILTERING##################
+medasum$all <- medasum$Gold.all + medasum$Silver.all + medasum$Bronze.all
+quantile(medasum$all)
+medasplit <- medasplit[medasplit$Country%in%(row.names(medasum[medasum$all>30.75,])),]
+###########################################
 ##################################################
-
+######################RAPID OVERVIEW###################"
 medaview <- medasplit[medasplit$Country%in%row.names(medasum)[medasum$Gold.all>400],]
 
 gg <- ggplot(medaview, aes(y=Medals, x=Date)) + 
   geom_crossbar(aes(ymin=0, ymax=Medals, col=Country)) +
-  labs(subtitle="Area Vs Population", 
-       y="Population", 
-       x="Area", 
-       title="Scatterplot", 
-       caption = "Source: midwest")
+  labs(subtitle="Country vs medals", 
+       y="Medals", 
+       x="Country", 
+       caption = "US vs Soviet Union")
 
 plot(gg)
 
 gg <- ggplot(subset(medasplit, Country=='South Korea' & Season=='Summer'), aes(y=Medals, x=Date)) + 
   geom_bar(stat = "identity", na.rm=TRUE, aes(color=HostCountry, fill=Rank), size=2) +
-  labs(subtitle="Area Vs Population", 
-       y="Population", 
-       x="Area", 
-       title="Scatterplot", 
-       caption = "Source: midwest")
+  labs(subtitle="Medals per year", 
+       y="Medals", 
+       x="Year", 
+       caption = "Sout Korea")
 
 plot(gg)
 ok <- plotly_build(gg)
 ok
+##################################################
+#######################SUMMER#####################
+summermedals <- subset(medasplit, Season=="Summer")
+
+gg <- ggplot(summermedals, aes(y=Medals, x=HostCountry)) + 
+  geom_jitter(aes(color=Country, shape=Rank), size=2) +
+  labs(subtitle="Medals per year", 
+       y="Medals", 
+       x="Year", 
+       caption = "Sout Korea")
+
+gg
+ok <- plotly_build(gg)
+ok
+
+medasplit$Date <- as.numeric(medasplit$Date)
+medasplit$Medals <- as.numeric(medasplit$Medals)
+
+
 
